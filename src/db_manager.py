34b729ffa00b4d1a7ed45143cbfa2fc54bd040c8
SilvanaSchulze <silvanaSchulze87@gmail.com>
2025-01-04 15:17:28 +0100

    Corrigida estrutura de arquivos: ajustados assets, docs e test

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,1 +1,36 @@
-# Gerenciamento do banco de dados
+import sqlite3
+
+DB_PATH = "candidaturas.db"
+
+def criar_tabelas():
+    conexao = sqlite3.connect(DB_PATH)
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS candidaturas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        data_candidatura TEXT NOT NULL,
+        email_enviado TEXT NOT NULL,
+        status_envio TEXT NOT NULL,
+        data_feedback TEXT,
+        resposta_feedback TEXT,
+        status_resposta TEXT,
+        data_entrevista TEXT,
+        metodo_entrevista TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS etapas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        candidatura_id INTEGER NOT NULL,
+        etapa_numero INTEGER NOT NULL,
+        data TEXT NOT NULL,
+        resultado TEXT NOT NULL,
+        FOREIGN KEY (candidatura_id) REFERENCES candidaturas (id)
+    )
+    """)
+
+    conexao.commit()
+    conexao.close()
+    print("Tabelas criadas com sucesso!")
--- src/main.py
+++ src/main.py
@@ -1,1 +1,26 @@
-# Arquivo principal
+from db_manager import criar_tabelas, adicionar_candidatura, listar_candidaturas, pesquisar_candidaturas 
+
+# Criar tabelas no banco de dados
+criar_tabelas()
+
+
+
+# adicionar_candidatura("20-12-2024", "empresa1@example.com", "Enviado", "25-12-2024", "Pendente")
+adicionar_candidatura(
+    "20-12-2024", "empresa1@example.com", "Enviado", "25-12-2024", "Pendente"
+)
+print("comente o primeiro teste")
+
+#lista Candidaturas
+candidaturas = listar_candidaturas()
+print("Lista de Candidaturas:")
+for candidatura in candidaturas:
+    print(candidatura)
+
+
+
+# Pesquisar candidaturas com status "Enviado"
+resultados = pesquisar_candidaturas("status_envio", "Enviado")
+print("Candidaturas com status 'Enviado':")
+for r in resultados:
+    print(r)
